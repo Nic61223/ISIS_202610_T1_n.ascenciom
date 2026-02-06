5304f29a16428b365b057450f407a67696be134f
n.ascenciom <n.ascenciom@uniandes.edu.co>
2026-02-06 12:28:15 -0500

    primera parte

---
+++ src/main/java/co/edu/uniandes/dse/TallerPersistencia/entities/DirectorEntity.java
@@ -0,0 +1,22 @@
+package co.edu.uniandes.dse.TallerPersistencia.entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+@Data
+@Entity
+
+public class DirectorEntity extends BaseEntity{
+    
+    private Long id;
+
+    private String nombre;
+    private String biografia;
+
+    @OneToMany( mappedBy = "peliculas")
+     private java.util.List<PeliculaEntity> peliculas;
+     
+
+
+    
+}
---
+++ src/main/java/co/edu/uniandes/dse/TallerPersistencia/entities/PeliculaEntity.java
@@ -0,0 +1,17 @@
+package co.edu.uniandes.dse.TallerPersistencia.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Data
+@Entity
+public class PeliculaEntity extends BaseEntity {
+    private Long id;
+    private String titulo;
+    private String anioLanzamiento;
+        @ManyToOne( mappedBy = "director")
+     private director director;
+}
